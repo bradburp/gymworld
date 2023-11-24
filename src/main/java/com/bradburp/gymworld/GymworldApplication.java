495b62807f1c30d77e57700edf9bffa9acba79a5
bradburp <paul.bradbury@live.co.uk>
2023-11-24 08:21:06 +0000

    Initial commit

---
+++ src/main/java/com/bradburp/gymworld/GymworldApplication.java
@@ -0,0 +1,13 @@
+package com.bradburp.gymworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymworldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymworldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bradburp/gymworld/GymworldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bradburp.gymworld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymworldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
